6bfbe78a7e8c20ba35de984de1642c5ca5599bea
ankhokhlov.sbt <ankhokhlov.sbt@sberbank.ru>
2023-09-18 14:49:48 +0300

    fix tests

---
+++ accounts/src/test/resources/sql/data.sql
@@ -0,0 +1,4 @@
+insert into account(id, balance, num, currency, person_id) values  (1000, 1000, '111', 'RUR', 1000);
+insert into account(id, balance, num, currency, person_id) values (2000, 2000, '222', 'RUR', 2000);
+insert into account(id, balance, num, currency, person_id) values (3000, 3000, '333', 'USD', 3000);
+insert into account(id, balance, num, currency, person_id) values (4000, 4000, '444', 'USD', 1000);
